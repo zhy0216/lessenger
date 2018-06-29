e9e3c87ce47700f9323574a3c3b0902e485b3d85
Yang <zhy0216@gmail.com>
2018-06-28 21:25:19 -0700

    add run application and run test guide

--- README.md
+++ README.md
@@ -7,7 +7,18 @@ It is a small code project practice.
 * python 3.6.5
 * Mac 10.13.5
 
+## Run Application
+* `virtualenv venv -p python3`
+* `source venv/bin/activate`
+* `pip install -r requirements.txt`
+* `export DARK_SKY_API_KEY=<YOUR KEY HERE>`
+* `export GMAP_API_KEY=<YOUR KEY HERE>`
+* `python web.py`
 
+## Run Test
+* `py.test tests/*`
+
+make sure all the test cases can pass before you run the application.
 
 ## Reference:
 
--- tests/integral_tests.py
+++ tests/integral_tests.py
@@ -21,3 +21,12 @@ async def test_fetcher_fetch_latlng_by_postcode():
         assert float_equal(latlng.lat, 37.7726402)
         assert float_equal(latlng.lng, -122.4099154)
 
+
+@pytest.mark.asyncio
+async def test_fetcher_fetch_current_weather_by_latlng():
+    async with aiohttp.ClientSession() as session:
+        fetcher = Fetcher(session)
+        latlng = await fetcher.fetch_latlng_by_postcode(94103)
+        weather = await fetcher.fetch_current_weather_by_latlng(latlng)
+        assert weather.summary
+        assert weather.temperature
